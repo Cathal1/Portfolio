8cbbe818342d1843d7aa9c3e7cde2e10ff7e13b1
Cathal Farrell <cathal.ynwa@gmail.com>
2013-12-15 19:20:48 +0000

    Initial html, css and javascript

---
+++ javascript/imagechange.js
@@ -0,0 +1,49 @@
+function onHoverHome()
+{
+    $("#menuImg1").attr('src', 'images/homebuttonhover.png');
+}
+
+function offHoverHome()
+{
+    $("#menuImg1").attr('src', 'images/homebutton.png');
+}
+
+function onHoverAbo()
+{
+    $("#menuImg2").attr('src', 'images/aboutbuttonhover.png');
+}
+
+function offHoverAbo()
+{
+    $("#menuImg2").attr('src', 'images/aboutbutton.png');
+}
+
+function onHoverCon()
+{
+    $("#menuImg4").attr('src', 'images/contactbuttonhover.png');
+}
+
+function offHoverCon()
+{
+    $("#menuImg4").attr('src', 'images/contactbutton.png');
+}
+
+function onHoverLin()
+{
+    $("#menuImg5").attr('src', 'images/linkedinbuttonhover.png');
+}
+
+function offHoverLin()
+{
+    $("#menuImg5").attr('src', 'images/linkedinbutton.png');
+}
+
+function onHoverGit()
+{
+    $("#menuImg6").attr('src', 'images/githubbuttonhover.png');
+}
+
+function offHoverGit()
+{
+    $("#menuImg6").attr('src', 'images/githubbutton.png');
+}
